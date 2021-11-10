02afad893100753559e728583a66ab8d8833b08a
sangit sigdel <sangit.sigdel@gmail.com>
2021-11-10 21:59:51 +0000

    some useful commit

--- build/server.js
+++ build/server.js
@@ -2,21 +2,23 @@
 var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mongoose = void 0;
-var app = require('./index');
-var dotenv = require('dotenv');
-exports.mongoose = require('mongoose');
-dotenv.config({ path: './config.env' });
+var app = require("./index");
+var dotenv = require("dotenv");
+exports.mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
 var db_password = process.env.DATABASE_PASSWORD;
-var DB = (_a = process.env.DATABASE) === null || _a === void 0 ? void 0 : _a.replace('<password>', db_password);
-exports.mongoose.connect(DB, {
+var DB = (_a = process.env.DATABASE) === null || _a === void 0 ? void 0 : _a.replace("<password>", db_password);
+exports.mongoose
+    .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 })
     .then(function con() {
-    console.log('database connected successfully');
-});
+    console.log("database connected successfully");
+})
+    .catch(function (error) { return console.log(error.message); });
 var PORT = process.env.PORT;
 var server = app.listen(PORT, function () {
     console.log("Server is running in the port " + PORT);
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,27 @@
-const app = require('./index')
-const dotenv = require('dotenv')
-export const mongoose = require('mongoose')
+const app = require("./index");
+const dotenv = require("dotenv");
+export const mongoose = require("mongoose");
 
-dotenv.config({ path: './config.env' })
+dotenv.config({ path: "./config.env" });
 
-const db_password = process.env.DATABASE_PASSWORD
+const db_password = process.env.DATABASE_PASSWORD;
 
-const DB = process.env.DATABASE?.replace('<password>', db_password!)
+const DB = process.env.DATABASE?.replace("<password>", db_password!);
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-})
-  .then(function con(): void {
-    console.log('database connected successfully')
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
   })
+  .then(function con(): void {
+    console.log("database connected successfully");
+  })
+  .catch((error) => console.log(error.message));
 
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server is running in the port ${PORT}`)
-})
+  console.log(`Server is running in the port ${PORT}`);
+});
